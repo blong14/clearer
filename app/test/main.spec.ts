971d9b49a73733b2015cdb4f65fbf6f779cae496
Josh Mobley <jmob1986@gmail.com>
2017-03-18 14:32:20 -0400

    moving rxjs module up the tree

--- app/test/main.spec.ts
+++ app/test/main.spec.ts
@@ -1,11 +1,11 @@
 import{ ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Observable } from 'rxjs/observable';
 
 
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { DataService } from '../data.service';
-import { Observable } from 'rxjs/observable';
 
 describe('DashboardComponent (external template)', function(){
 
