9ba0e46c32efc60ac0d3b12ae20562cd5fa37cde
Josh Mobley <josh@joshmobley.net>
2017-05-23 14:37:43 -0400

    built out signup screen and hoooked into firebase to be able to create users on the fly

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
+import { FirebaseAuth } from 'angularfire2';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -11,7 +12,7 @@ import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'a
 @Injectable()
 export class DataService {
 
-    constructor( private af: AngularFire, private http: Http ) { }
+    constructor( private af: AngularFire, private http: Http, private router: Router ) { }
 
     getData(): FirebaseListObservable<any>{
 
@@ -51,6 +52,24 @@ export class DataService {
 
     getUsers() : FirebaseListObservable<any[]>{
         return this.af.database.list('/users');
+    }
+
+    createUser( name, email, password ) {
+      this.af.auth.createUser({
+        'email': email,
+        'password': password
+      }).then( (user) => {
+        return this.af.database.object('/users/' + user.uid ).update({
+          'name': name,
+          'email': email,
+          'provider': 'password'
+        }).then( () => {
+          this.router.navigate(['/dashboard']);
+        });
+      }).catch( (err) => {
+        console.log(err.message);
+      });
+
     }
 
 }
--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { SignupComponent } from './signup.component';
@@ -9,6 +10,7 @@ import { NotAuthGuardService } from '../not-auth-guard.service';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild([
       { path: 'signup', component: SignupComponent, canActivate: [NotAuthGuardService] }
     ])
