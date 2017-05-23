8c1aa0d41ff89874bc9220932e846c12e55e0047
Josh Mobley <josh@joshmobley.net>
2017-05-22 22:34:35 -0400

    adding authguards to prevent reaching parts of the site unless auth'd

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard.component';
+
+import { AuthGuardService } from '../auth-guard.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'dashboard', component: DashboardComponent, canActivate: [ AuthGuardService ] },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ])
   ],
-  declarations: []
+  declarations: [ DashboardComponent ],
+  providers: [ AuthGuardService ]
 })
 export class DashboardModule { }
