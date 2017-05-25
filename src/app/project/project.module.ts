acf0c6c737598407fbfe7b5bed6ebfcbab6980fe
Josh Mobley <josh@joshmobley.net>
2017-05-24 22:51:54 -0400

    adding create a project tiles to views

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProjectComponent } from './project.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ProjectComponent]
 })
 export class ProjectModule { }
