6c80c02013e9930cb8e727a5601c46ed69c51c71
Josh Mobley <josh@joshmobley.net>
2017-08-18 09:42:03 -0400

    adding server redirect back in

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
