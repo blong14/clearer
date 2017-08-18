ce312552702cfed1801ccef8c7f8e150baa7392c
Josh Mobley <josh@joshmobley.net>
2017-08-18 00:01:49 -0400

    tweaks to server config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 /*const forceSSL = function() {
   return function (req, res, next) {
@@ -17,9 +18,9 @@ const app = express();
 app.use(forceSSL());
 */
 
-app.get('/*', function(req, res) {
+/*app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+});*/
 
 // Run the app by serving the static files
 // in the dist directory
